a0bb8cc5ffdc2e01d4a8fc7416f4f69a493042cf
divyanshubhushan <divyanshubhushan@users.noreply.github.com>
2016-02-20 14:00:25 +0530

    Added files via upload

--- pageportion/quantity.php
+++ pageportion/quantity.php
@@ -1,14 +1,14 @@
 <div class="different_filters_divbox">                                            
 	<ul class="different_filters">
 		<?php		
-		$sizearray = $db->getResults('tbl_sizes');		
+		$sizearray = $db->getResults('volume');		
 		foreach($sizearray as $size) {
-			$sizee = $size['size'];	
-			$material_id = $size['id'];
-			$number_in_category = $db->number_in_category('tbl_productsizes', $material_id, 'sizeID');	
+			$sizee = $size['volumesize'];	
+			//$material_id = $size['Brand'];
+			$number_in_category = $db->number_in_category('updatedtables',$sizee, 'volume');	
 		?>		
 		<ul style="padding:0">
-			<input type="checkbox" id="size-<?=strtolower($sizee);?>" name="scheck" class="scheck" value="<?=$size['id']?>"/>
+			<input type="checkbox" id="size-<?=strtolower($sizee);?>" name="quancheck" class="quancheck" value="<?=$size['id']?>"/>
 			<label for="size-<?=strtolower($sizee);?>"><?=$sizee?> (<?=$number_in_category?>)</label>
 		</ul>
 		
